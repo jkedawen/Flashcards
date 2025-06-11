4463de4a9267ecf4eec5c8e00f8b41a682c5a02d
Jonathan Kedawen <kedawenjr123@gmail.com>
2025-06-11 20:44:41 +0800

    Added working flashcard app code

---
+++ script.js
@@ -0,0 +1,46 @@
+const questionInput = document.getElementById('question');
+const answerInput = document.getElementById('answer');
+const addCardBtn = document.getElementById('addCardBtn');
+const cardsContainer = document.getElementById('cards-container');
+
+let flashcards = JSON.parse(localStorage.getItem('flashcards')) || [];
+
+function saveAndRenderCards() {
+  localStorage.setItem('flashcards', JSON.stringify(flashcards));
+  renderCards();
+}
+
+function renderCards() {
+  cardsContainer.innerHTML = '';
+  flashcards.forEach((card, index) => {
+    const cardDiv = document.createElement('div');
+    cardDiv.className = 'card';
+
+    cardDiv.innerHTML = `
+      <strong>Q:</strong> ${card.question} <br>
+      <strong>A:</strong> ${card.answer}
+      <span class="delete" onclick="deleteCard(${index})">&times;</span>
+    `;
+
+    cardsContainer.appendChild(cardDiv);
+  });
+}
+
+function deleteCard(index) {
+  flashcards.splice(index, 1);
+  saveAndRenderCards();
+}
+
+addCardBtn.addEventListener('click', () => {
+  const question = questionInput.value.trim();
+  const answer = answerInput.value.trim();
+
+  if (question && answer) {
+    flashcards.push({ question, answer });
+    questionInput.value = '';
+    answerInput.value = '';
+    saveAndRenderCards();
+  }
+});
+
+renderCards(); // initial load
